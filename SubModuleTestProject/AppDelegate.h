38abd39bf015b1cb8eb2fbe42917ba537d48f15c
Sravan Gangisetty <venkata_harisravankumarraju_gangisetty@apple.com>
2017-06-22 14:52:22 -0700

    first commit

---
+++ SubModuleTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SubModuleTestProject
+//
+//  Created by Venkata Hari Sravan Kumar Raju on 6/22/17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
